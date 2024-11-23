cda58c325b4a1ca78587f887b52a3cf80d73f851
Seth Collins <blismatic@gmail.com>
2024-11-22 17:48:41 -0800

    solve day 03 part 2

--- 03/main.py
+++ 03/main.py
@@ -10,19 +10,37 @@ def parse(puzzle_input: str):
     """Parse input."""
     result = puzzle_input.split("\n")
 
-    pprint(result)
+    pprint(result[:3])
     print()
     return result
 
 
-def part1(data):
+def part1(data: list[str], slope: tuple[int, int] = (3, 1)) -> int:
     """Solve and return the answer to part 1."""
-    pass
+    length = len(data[0])  # This refers to the length of a single row before it begins re-starting
+    start_pos = {"x": 0, "y": 0}
+    curr_pos = start_pos
+    slope = {"x": slope[0], "y": slope[1]}
+
+    num_trees = 0
+    for row in data[:: slope["y"]]:
+        if row[curr_pos["x"] % length] == "#":
+            num_trees += 1
+        curr_pos["x"] += slope["x"]
+        curr_pos["y"] -= slope["y"]  # Assume that moving down the map decreases your y-value
+
+    return num_trees
 
 
-def part2(data):
+def part2(data: list[str]) -> int:
     """Solve and return the answer to part 2."""
-    pass
+    slopes = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
+
+    num_trees = [part1(data, slope=s) for s in slopes]
+    result = 1
+    for tree in num_trees:
+        result *= tree
+    return result
 
 
 def solve(puzzle_input) -> tuple:
@@ -37,7 +55,7 @@ def solve(puzzle_input) -> tuple:
 if __name__ == "__main__":
     load_dotenv()
     # solutions = solve(example_input)
-    puzzle_input = get_data(day=1, year=2020)
+    puzzle_input = get_data(day=3, year=2020)
     solutions = solve(puzzle_input)
 
     print("\n".join(str(solution) for solution in solutions))
